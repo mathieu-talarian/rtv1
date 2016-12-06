cb23b28d4d78662a78eec7b7078845061d4c7046
Mathieu MOULLEC <mmoullec@e2r7p5.42.fr>
2016-12-06 17:17:27 +0100

    pas fait grand chose

--- test.c
+++ test.c
@@ -8,19 +8,20 @@ typedef struct	s_eq_plan
 	float		d;
 }				t_eq_plan;
 
-//t_eq_plan		find_vp_eq(t_vect n, t_vect a)
-//{
-//	t_eq_plan pl;
+t_eq_plan		find_vp_eq(t_vect n, t_vect a)
+{
+	t_eq_plan pl;
 
-//}
+}
 
 int main(void)
 {
 	t_eq_plan viewplane;
-	t_vect cam_pos = vect_create(0, 2, 1);
+	t_vect cam_pos = vect_create(2, 6, 3);
 	t_vect origin = vect_create(0, 0, 0);
 
-
+	t_vect pt_exemple = vect_create(1, 2, 3);
+	t_vect ray_n_exemple = vect_create(-1, 3, 5);
 
 	t_vect ray_cam_or = vect_sub(origin, cam_pos);
 	t_vect ray_unit_cam_or = vect_unit(ray_cam_or);
@@ -29,7 +30,6 @@ int main(void)
 	pr_vect(ray_cam_or, "RAY CAM ORIGIN\n");
 	pr_vect(ray_unit_cam_or, "RAY CAM ORIGIN UNIT\n");
 
-//	viewplane = find_vp_eq(ray_cam_or, pt_plan);
-
+	viewplane = find_vp_eq(ray_n_exemple, pt_exemple);
 	return (0);
 }
